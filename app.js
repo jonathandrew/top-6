eb48d32fa8396d40d229ec8c70c5103854d8609e
Ryan White <mrwhiterk@gmail.com>
2019-05-20 13:54:59 -0400

    formatted code

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ let modal = document.getElementById('myModal');
 let modalContent = document.querySelector('.modal-content');
 
 let btn = document.querySelectorAll('.links li');
+
 btn.forEach(a => {
   a.addEventListener('click', () => {
     let elem = document.querySelector('img');
+
     if (elem) {
       elem.parentNode.removeChild(elem);
     }
 
     let imageSrc = a.getAttribute('data-src');
-    console.log(imageSrc);
-
     let image = document.createElement('img');
     image.src = imageSrc;
-
-    console.log(image);
 
     modalContent.append(image);
     modal.style.display = 'block';
